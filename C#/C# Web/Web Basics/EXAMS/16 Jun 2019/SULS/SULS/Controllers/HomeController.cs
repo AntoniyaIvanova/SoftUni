93a05a58b39b888920280d528e74e2e5473c10c6
Antoniya Ivanova <a.ivanova86@yahoo.com>
2020-04-26 13:13:03 +0300

    Create models

---
+++ C#/C# Web/Web Basics/EXAMS/16 Jun 2019/SULS/SULS/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using SIS.HTTP;
+using SIS.MvcFramework;
+
+namespace SULS.App.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("/")]
+        public HttpResponse IndexSlash()
+        {
+            return this.Index();
+        }
+
+        public HttpResponse Index()
+        {
+            return this.View();
+        }
+    }
+}
---
+++ C#/C# Web/Web Basics/EXAMS/16 Jun 2019/SULS/SULS/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿namespace SULS
+{
+    using Microsoft.EntityFrameworkCore;
+    using SULS.Models;
+
+    public class ApplicationDbContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer(DatabaseConfiguration.ConnectionString);
+        }
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Problem> Problems { get; set; }
+
+        public DbSet<Submission> Submissions { get; set; }
+               
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
