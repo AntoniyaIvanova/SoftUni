2e98b4612981d10831637d4fd6e8af421c0ffa5e
Antoniya Ivanova <a.ivanova86@yahoo.com>
2020-04-28 21:01:07 +0300

    Exam changes

--- C#/C# Web/Web Basics/EXAMS/16 Jun 2019/SulsApp/Data/DatabaseConfiguration.cs
+++ C#/C# Web/Web Basics/EXAMS/16 Jun 2019/SulsApp/Data/DatabaseConfiguration.cs
@@ -3,6 +3,6 @@
     public class DatabaseConfiguration
     {
         public const string ConnectionString =
-            @"Server=DESKTOP-LPLN6BV\SQLEXPRESS;Database=SULS;Trusted_Connection=True;Integrated Security=True;";
+            @"Server=.;Database=SULS;Trusted_Connection=True;Integrated Security=True;";
     }
 }
