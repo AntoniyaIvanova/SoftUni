cbdb44550dbb7ad61b055daa825ede24b24d56fe
Antoniya Ivanova <a.ivanova86@yahoo.com>
2020-04-28 20:55:23 +0300

    ....

---
+++ C#/C# Web/Web Basics/EXAMS/16 Jun 2019/SulsApp/Data/DatabaseConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace SulsApp.Data
+{
+    public class DatabaseConfiguration
+    {
+        public const string ConnectionString =
+            @"Server=DESKTOP-LPLN6BV\SQLEXPRESS;Database=SULS;Trusted_Connection=True;Integrated Security=True;";
+    }
+}
