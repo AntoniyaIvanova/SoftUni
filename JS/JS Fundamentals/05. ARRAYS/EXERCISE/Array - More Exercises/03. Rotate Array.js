1ea8e02e719faec447de11dca97b1ee2bfa76cbb
Antoniya Ivanova <a.ivanova86@yahoo.com>
2020-05-13 20:14:48 +0300

    03. Rotate array - 100/100

---
+++ JS/JS Fundamentals/05. ARRAYS/EXERCISE/Array - More Exercises/03. Rotate Array.js
@@ -0,0 +1,12 @@
+function rotateArray(arr) {
+
+    let rotations = arr.pop();
+
+    for (let i = 0; i < rotations; i++) {
+        let lastElement = arr.pop();
+        arr.unshift(lastElement);
+    }
+    console.log(arr.join(" "));
+}
+
+rotateArray(['Banana', 'Orange', 'Coconut', 'Apple', '15']);
