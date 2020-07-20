50a5bdf3ea64e6290afede8b01e691f6f228a5b4
Antoniya Ivanova <a.ivanova86@yahoo.com>
2020-07-20 23:45:18 +0300

    04. Largest Number - 100/100

---
+++ JS/JS Essentials/Syntax, Functions and Statements/LAB/04. Largest Number.js
@@ -0,0 +1,16 @@
+function largestNumber(arg1, arg2, arg3) {
+
+    let result;
+
+    if (arg1 > arg2 && arg1 > arg3) {
+        result = arg1;
+    }
+    else if (arg2 > arg1 && arg2 > arg3) {
+        result = arg2;
+    }
+    else {
+        result = arg3;
+    }
+
+    console.log(`The largest number is ${result}.`);
+}
