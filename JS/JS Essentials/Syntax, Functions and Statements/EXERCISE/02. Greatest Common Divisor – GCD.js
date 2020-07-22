40a95f66d2104de391cf241fb06a6a08b18abf1c
Antoniya Ivanova <a.ivanova86@yahoo.com>
2020-07-22 22:10:53 +0300

    02. Greatest Common Divisor - 100/100

---
+++ JS/JS Essentials/Syntax, Functions and Statements/EXERCISE/02. Greatest Common Divisor – GCD.js
@@ -0,0 +1,9 @@
+function greatestCommonDiv(firstNumber, secondNumber) {
+
+    while (secondNumber) {
+        let thirdNumber = secondNumber;
+        secondNumber = firstNumber % secondNumber;
+        firstNumber = thirdNumber;
+    }
+    console.log(firstNumber);
+}
