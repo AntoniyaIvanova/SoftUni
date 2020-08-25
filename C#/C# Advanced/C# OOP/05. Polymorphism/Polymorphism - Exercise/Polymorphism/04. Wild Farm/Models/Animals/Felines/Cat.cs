52d188851479f4e83cb390c454d21999e497f656
Antoniya Ivanova <a.ivanova86@yahoo.com>
2020-08-25 23:58:38 +0300

    04. Wild Farm - 100/100

---
+++ C#/C# Advanced/C# OOP/05. Polymorphism/Polymorphism - Exercise/Polymorphism/04. Wild Farm/Models/Animals/Felines/Cat.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WildFarm.Models.Foods;
+
+namespace WildFarm.Models.Animals
+{
+    public class Cat : Feline
+    {
+        private const double Increase = 0.30;
+
+        public Cat(string name, double weight,string livingRegion, string breed)
+            :base(name, weight, livingRegion, breed)
+        {
+
+        }
+
+        public override void Eat(Food food)
+        {
+            this.BaseEat(food, new List<string>() { nameof(Meat), nameof(Vegetable) }, Increase);
+        }
+
+        public override string ProduceSound()
+        {
+            return $"Meow";
+        }
+
+
+    }
+}
