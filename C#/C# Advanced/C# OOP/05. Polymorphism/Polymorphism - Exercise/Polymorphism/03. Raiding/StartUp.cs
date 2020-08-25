e713d9d91e64ae445086d1fd57770c1b2031804c
Antoniya Ivanova <a.ivanova86@yahoo.com>
2020-08-26 00:01:36 +0300

    03. Raiding - 100/100

---
+++ C#/C# Advanced/C# OOP/05. Polymorphism/Polymorphism - Exercise/Polymorphism/03. Raiding/StartUp.cs
@@ -0,0 +1,11 @@
+﻿namespace _03._Raiding
+{
+    using System;
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
