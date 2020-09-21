bd368ad185ef4f32c69cb8aa0940354f7a59da88
Antoniya Ivanova <a.ivanova86@yahoo.com>
2020-09-21 22:09:47 +0300

    03. Negative/Positive Numbers - 100/100

---
+++ JS/JS Advanced/JS Advanced/Arrays/LAB/03. Negative  Positive Numbers/negative positive numbers.js
@@ -0,0 +1,14 @@
+function solve(array) {
+    let newArray = [];
+
+    while (array.length > 0) {
+        let element = array.shift();
+        if (element >= 0) {
+            newArray.push(element);
+        } else {
+            newArray.unshift(element);
+        }
+    }
+    console.log(newArray.join("\r\n"));
+}
+solve([7, -2, 8, 9]);
