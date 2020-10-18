dd391993b719f3e3a388cd5f363b533de587b9f1
Antoniya Ivanova <a.ivanova86@yahoo.com>
2020-10-18 20:41:18 +0300

    01. Sort Array - 100/100

---
+++ JS/JS Advanced/JS Advanced/07. Advanced Functions/EXERCISE/01. Sort Array/sort.js
@@ -0,0 +1,12 @@
+function sortArrayFunction(array, sortType) {
+    if (sortType === 'asc') {
+        return array.sort((a, b) => {
+            return a - b
+        })
+    }
+    else {
+        return array.sort((a, b) => {
+            return b - a
+        })
+    }
+}
