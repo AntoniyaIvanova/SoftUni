2bb2b8f7427b67de597930dca9ce6254811f99f3
Antoniya Ivanova <a.ivanova86@yahoo.com>
2020-11-24 23:04:59 +0200

    Done

--- JS/JS Advanced/JS Applications/07. WORKSHOP/EXERCISE/index.js
+++ JS/JS Advanced/JS Applications/07. WORKSHOP/EXERCISE/index.js
@@ -1,13 +1,20 @@
-const UserModel = firebase.auth;
+const UserModel = firebase.auth();
+const dbContext = firebase.firestore();
 
 const app = Sammy('#root', function () {
     this.use('Handlebars', 'hbs');
 
     this.get('/home', function (context) {
-        loadPartialsToContext(context)
-            .then(function () {
-                this.partial('./templates/homeGuest.hbs');
+        dbContext.collection('offers')
+            .get()
+            .then((res) => {
+                context.offers = res.docs.map((offer => { return { id: offer.id, ...offer.data() } }));
+                loadPartialsToContext(context)
+                    .then(function () {
+                        this.partial('./templates/home.hbs');
+                    })
             })
+            .catch((e) => console.log(e));
     })
 
     this.get('/register', function (context) {
@@ -26,7 +33,7 @@ const app = Sammy('#root', function () {
 
         UserModel.createUserWithEmailAndPassword(email, password)
             .then((userdata) => {
-                this.redirect('/home');
+                this.redirect('/login');
             })
             .catch((e) => console.log(e));
     });
@@ -49,13 +56,13 @@ const app = Sammy('#root', function () {
             .catch((e) => console.log(e));
     });
 
-    this.get('/logout', function(){
+    this.get('/logout', function () {
         UserModel.signOut()
-        .then((res)=>{
-            clearUserData();
-            this.redirect('/home');
-        })
-        .catch((e)=>console.log(e));
+            .then((res) => {
+                clearUserData();
+                this.redirect('/home');
+            })
+            .catch((e) => console.log(e));
     })
 
     this.get('/create-offer', function (context) {
@@ -65,21 +72,119 @@ const app = Sammy('#root', function () {
             })
     })
 
-    this.get('/edit-offer', function (context) {
-        loadPartialsToContext(context)
-            .then(function () {
-                this.partial('./templates/editOffer.hbs');
+    this.post('/create-offer', function (context) {
+        const { productName, price, imageUrl, description, brand } = context.params;
+        console.log(context.params);
+
+        dbContext.collection("offers").add({
+            productName,
+            price,
+            imageUrl,
+            description,
+            brand,
+            salesman: getUserData().uid,
+            clients: [],
+        })
+            .then((createdProduct) => {
+                console.log(createdProduct);
+                this.redirect('/home');
+            })
+            .catch((e) => console.log(e));
+    })
+
+    this.get('#/details/:offerId', function (context) {
+        const { offerId } = context.params;
+        dbContext.collection('offers')
+            .doc(offerId)
+            .get()
+            .then((response) => {
+                const { uid } = getUserData();
+                const actualOfferData = response.data();
+                const imTheSalesman = actualOfferData.salesman === uid;
+                const userIndex = actualOfferData.clients.indexOf(uid);
+                console.log(userIndex);
+                const imInTheClientsList = userIndex > -1;
+                context.offer = { ...actualOfferData, imTheSalesman, id: offerId, imInTheClientsList };
+                loadPartialsToContext(context)
+                    .then(function () {
+                        this.partial('./templates/details.hbs');
+                    })
             })
     })
 
-    this.get('/details', function (context) {
-        loadPartialsToContext(context)
-            .then(function () {
-                this.partial('./templates/details.hbs');
-            })
+    this.get('#/delete/:offerId', function (context) {
+        const { offerId } = context.params;
+
+        dbContext
+            .collection('offers')
+            .doc(offerId)
+            .delete()
+            .then(() => this.redirect('/home'))
+            .catch((e) => console.log(e));
     })
 
+    this.get('#/edit/:offerId', function (context) {
+        const { offerId } = context.params;
 
+        dbContext.collection('offers')
+            .doc(offerId)
+            .get()
+            .then((res) => {
+                context.offer = { id: offerId, ...res.data() };
+                loadPartialsToContext(context)
+                    .then(function () {
+                        this.partial('./templates/editOffer.hbs');
+                    });
+            });
+
+    })
+
+    this.post('#/edit/:offerId', function (context) {
+        const { offerId, productName, price, brand, imageUrl, description } = context.params;
+
+        dbContext.collection('offers')
+            .doc(offerId)
+            .get()
+            .then((response) => {
+
+                return dbContext.collection('offers')
+                    .doc(offerId)
+                    .set({
+                        ...response.data(),
+                        productName,
+                        price,
+                        brand,
+                        imageUrl,
+                        description
+                    })
+
+            })
+            .then((resp) => {
+                this.redirect(`#/details/${offerId}`);
+            })
+            .catch((e) => console.log(e));
+    });
+
+    this.get('#/buy/:offerId', function (context) {
+        const { offerId } = context.params;
+        const { uid } = getUserData();
+
+        dbContext.collection('offers')
+            .doc(offerId)
+            .get()
+            .then((response) => {
+                const offerData = { ...response.data() };
+                offerData.clients.push(uid)
+
+                return dbContext.collection('offers')
+                    .doc(offerId)
+                    .set(offerData)
+            })
+            .then(() => {
+                this.redirect(`#/details/${offerId}`);
+            })
+            .catch((e) => console.log(e));
+    })
 });
 
 (() => {
@@ -87,8 +192,7 @@ const app = Sammy('#root', function () {
 })();
 
 function loadPartialsToContext(context) {
-
-    let user = getUserData();
+    const user = getUserData();
     context.isLoggedIn = Boolean(user);
     context.email = user ? user.email : '';
 
@@ -99,16 +203,16 @@ function loadPartialsToContext(context) {
 }
 
 function saveUserData(data) {
-    const { user: {email, uid} } = data;
-    localStorage.setItem('user', JSON.stringify(email, uid));
+    const { user: { email, uid } } = data;
+    localStorage.setItem('user', JSON.stringify({ email, uid }));
 }
 
-function getUserData(){
+function getUserData() {
     const user = localStorage.getItem('user');
 
     return user ? JSON.parse(user) : null;
 }
 
-function clearUserData(){
-    this.localStorage.removeItem('user');
+function clearUserData() {
+    localStorage.removeItem('user');
 }
