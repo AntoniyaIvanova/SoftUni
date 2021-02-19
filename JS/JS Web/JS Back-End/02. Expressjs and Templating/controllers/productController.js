676c37f67f1d5b0cbf47868138abc7cb00b6ca92
Antoniya Ivanova <a.ivanova86@yahoo.com>
2021-02-19 23:32:09 +0200

    Add details page

--- JS/JS Web/JS Back-End/02. Expressjs and Templating/controllers/productController.js
+++ JS/JS Web/JS Back-End/02. Expressjs and Templating/controllers/productController.js
@@ -3,11 +3,16 @@ const { Router } = require('express');
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.render('home');
+    res.render('home', {title: 'Browse'});
 });
 
 router.get('/create', (req, res) => {
-    res.render('create');
+    res.render('create', {title: 'Create'});
 });
+
+router.get('/details/:productId', (req, res) => {
+    res.render('details', {title: 'Product Details'});
+});
+
 
 module.exports = router;
