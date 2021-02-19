7a1e9777b1ba44677c9d8e2a34ed02925c5d5341
Antoniya Ivanova <a.ivanova86@yahoo.com>
2021-02-19 22:26:14 +0200

    Add product, about and not found pages

--- JS/JS Web/JS Back-End/02. Expressjs and Templating/index.js
+++ JS/JS Web/JS Back-End/02. Expressjs and Templating/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
-const config = require('./config/config');
-
+const routes = require('./routes');
 const app = express();
+
+const config = require('./config/config');
+const expressConfig = require('./config/express');
+
+expressConfig(app);
+
+app.use(routes);
 
 app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT}...`));
---
+++ JS/JS Web/JS Back-End/02. Expressjs and Templating/routes.js
@@ -0,0 +1,15 @@
+
+const { Router } = require('express');
+
+const productController = require('./controllers/productController');
+const aboutController = require('./controllers/aboutController');
+
+const router = Router();
+
+router.use('/about', aboutController);
+router.use('/', productController);
+router.get('*', (req, res) => {
+    res.render('404', { layout: false });
+})
+
+module.exports = router;
