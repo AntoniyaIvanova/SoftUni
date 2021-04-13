0219c55ca8514e16c3d927b926f3e7c9c4b9d8cd
Antoniya Ivanova <a.ivanova86@yahoo.com>
2021-04-13 11:10:00 +0300

    Sessions and authorization

--- JS/JS Web/JS Back-End/02. Expressjs and Templating/models/User.js
+++ JS/JS Web/JS Back-End/02. Expressjs and Templating/models/User.js
@@ -5,10 +5,29 @@ const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
+        unique: true,
+        minlength: 5,
+        validate: {
+            validator: (value) => {
+                return /^[A-Za-z0-9]+$/.test(value);
+            },
+            message: (props) => {
+                return `${props.value} is invalid username. Username should consist of English letters and numbers.`
+            }
+        }
     },
     password: {
         type: String,
         required: true,
+        minlength: 8,
+        validate: {
+            validator: (value) => {
+                return /^[A-Za-z0-9]+$/.test(value);
+            },
+            message: (props) => {
+                return `${props.value} is invalid username. Username should consist of English letters and numbers.`
+            }
+        }
     }
 });
 
